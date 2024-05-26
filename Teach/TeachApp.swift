29b1281bbdb4e450c4d23de557e5e3d836b2085f
yj1722 <yj1722@ic.ac.uk>
2024-05-26 14:29:02 +0100

    Initial Commit

---
+++ Teach/TeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeachApp.swift
+//  Teach
+//
+//  Created by Davie on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
