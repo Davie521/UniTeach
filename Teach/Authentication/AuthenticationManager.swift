ce52a73eaf1619901f35f4d3cc92a31ccd8439d6
yj1722 <yj1722@ic.ac.uk>
2024-05-30 17:08:15 +0100

    delete the courseEditView

--- Teach/Authentication/AuthenticationManager.swift
+++ Teach/Authentication/AuthenticationManager.swift
@@ -1,8 +1,53 @@
+
 //
 //  AuthenticationManager.swift
 //  Teach
 //
-//  Created by Davie on 30/05/2024.
+//  Created by Davie on 26/05/2024.
 //
 
 import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel {
+    let uid: String
+    let email: String
+    let photoUrl: String?
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email!
+        self.photoUrl = user.photoURL?.absoluteString
+    }
+}
+
+final class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    private init() {}
+    
+    @discardableResult
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authResult.user)
+    }
+    
+    @discardableResult
+    func logInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authResult.user)
+    }
+    
+    func getAuthenticatedUser() throws -> AuthDataResultModel {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        return AuthDataResultModel(user: user)
+    }
+    
+    func logOut() throws {
+        try Auth.auth().signOut()
+    }
+    
+}
+
